2df01a970ca36e72cb3ccbbc2d51bb2775895491
Roman <meynewo@gmail.com>
2025-03-03 17:25:39 +0700

    add collision box

--- Source/FlappyBird/Private/Character/BaseCharacter.cpp
+++ Source/FlappyBird/Private/Character/BaseCharacter.cpp
@@ -4,26 +4,39 @@
 #include "Components/MyCharacterMovementComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Levels/LevelGenerator.h"
-#include "Engine/World.h"
 #include "EngineUtils.h"
+#include "Components/BoxComponent.h"
 
-ABaseCharacter::ABaseCharacter(const FObjectInitializer& ObjInit)
-    : Super(ObjInit.SetDefaultSubobjectClass<UMyCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
+ABaseCharacter::ABaseCharacter(const FObjectInitializer& ObjInit): Super(
+    ObjInit.SetDefaultSubobjectClass<UMyCharacterMovementComponent>(ACharacter::CharacterMovementComponentName))
 {
     PrimaryActorTick.bCanEverTick = true;
 
-    GetCharacterMovement()->GravityScale = 1.0f;
-
+    // Создаём FlipbookComponent
     FlipbookComponent = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("FlipbookComponent"));
     FlipbookComponent->SetupAttachment(RootComponent);
 
+    // Добавляем SpringArm
     SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>("SpringArmComponent");
-    SpringArmComponent->SetupAttachment(GetRootComponent());
+    SpringArmComponent->SetupAttachment(RootComponent);
     SpringArmComponent->bUsePawnControlRotation = true;
     SpringArmComponent->SocketOffset = FVector(0.0f, 100.0f, 80.0f);
 
+    // Добавляем Камеру
     CameraComponent = CreateDefaultSubobject<UCameraComponent>("CameraComponent");
     CameraComponent->SetupAttachment(SpringArmComponent);
+
+    // Добавляем BoxComponent для коллизии
+    CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+    CollisionBox->SetupAttachment(RootComponent);
+    CollisionBox->SetBoxExtent(FVector(40.0f, 40.0f, 40.0f));
+    CollisionBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+    CollisionBox->SetCollisionObjectType(ECollisionChannel::ECC_Pawn);
+    CollisionBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+    CollisionBox->SetGenerateOverlapEvents(true);
+
+    // Привязываем обработку события оверлапа
+    CollisionBox->OnComponentBeginOverlap.AddDynamic(this, &ABaseCharacter::OnOverlapBegin);
 }
 
 void ABaseCharacter::BeginPlay()
@@ -31,16 +44,17 @@ void ABaseCharacter::BeginPlay()
     Super::BeginPlay();
 
     FlipbookComponent->SetSimulatePhysics(true);
+    FlipbookComponent->SetEnableGravity(true);
+    GetCharacterMovement()->GravityScale = 1.0f;
+
     FlipbookComponent->BodyInstance.bLockXTranslation = true;
     FlipbookComponent->BodyInstance.bLockYTranslation = true;
-    FlipbookComponent->BodyInstance.bLockZTranslation = true;
+    FlipbookComponent->BodyInstance.bLockZTranslation = false;
 
-
-
-    // Автоматически ищем LevelGenerator, если он не задан вручную
+    // Автоматический поиск генератора уровня
     if (!LevelGenerator)
     {
-        for (TActorIterator<ALevelGenerator> It(GetWorld()); It;)
+        for (TActorIterator<ALevelGenerator> It(GetWorld()); It; ++It)
         {
             LevelGenerator = *It;
             break;
@@ -55,6 +69,42 @@ void ABaseCharacter::BeginPlay()
             UE_LOG(LogTemp, Error, TEXT("LevelGenerator not found!"));
         }
     }
+}
+
+void ABaseCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+
+    // Ограничиваем движение по оси X
+    FVector CurrentLocation = GetActorLocation();
+    CurrentLocation.X = 0.0f;
+    SetActorLocation(CurrentLocation);
+
+    // Перемещение персонажа вперёд
+    const FVector ForwardMovement = FVector(0.0f, 450.0f * DeltaTime, 0.0f);
+    AddActorWorldOffset(ForwardMovement, true);
+
+    // Обновляем уровень
+    if (LevelGenerator)
+    {
+        FVector CameraPos = SpringArmComponent->GetComponentLocation();
+        if (CameraPos != FVector::ZeroVector)
+        {
+            LevelGenerator->UpdateLevelGeneration(CameraPos);
+        }
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("LevelGenerator is nullptr!"));
+    }
+}
+
+void ABaseCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    check(PlayerInputComponent);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABaseCharacter::Jump);
 }
 
 void ABaseCharacter::Jump()
@@ -72,39 +122,12 @@ void ABaseCharacter::Jump()
     Super::Jump();
 }
 
-void ABaseCharacter::Tick(float DeltaTime)
+void ABaseCharacter::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                                    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+                                    bool bFromSweep, const FHitResult& SweepResult)
 {
-    Super::Tick(DeltaTime);
-
-    // Запрет передвижения по координате X
-    FVector CurrentLocation = GetActorLocation();
-    CurrentLocation.X = 0.0f;
-    SetActorLocation(CurrentLocation);
-    
-    // Смещение персонажа вперёд
-    const FVector ForwardMovement = FVector(0.0f, 450.0f * DeltaTime, 0.0f);
-    AddActorWorldOffset(ForwardMovement, true);
-
-    // Получение позиции камеры (через SpringArm)
-    FVector CameraPos = SpringArmComponent->GetComponentLocation();
-    if (CameraPos == FVector::ZeroVector) return;
-
-    // Обновление генерации уровня
-    if (LevelGenerator)
+    if (OtherActor && OtherActor != this) // Проверяем, чтобы это не был сам персонаж
     {
-        LevelGenerator->UpdateLevelGeneration(CameraPos);
+        UE_LOG(LogTemp, Warning, TEXT("HIT! Overlapped with: %s"), *OtherActor->GetName());
     }
-    else
-    {
-        UE_LOG(LogTemp, Error, TEXT("LevelGenerator is nullptr!"));
-    }
-}
-
-void ABaseCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
-{
-    Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-    check(PlayerInputComponent);
-
-    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ABaseCharacter::Jump);
 }
--- Source/FlappyBird/Public/Character/BaseCharacter.h
+++ Source/FlappyBird/Public/Character/BaseCharacter.h
@@ -1,15 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "BaseCharacter.generated.h"
 
+// Предварительные декларации для указателей
 class ALevelGenerator;
 class USpringArmComponent;
 class UPaperFlipbookComponent;
 class UCameraComponent;
+class UBoxComponent; // Не хватает объявления этого компонента в вашем коде
 
 UCLASS()
 class FLAPPYBIRD_API ABaseCharacter : public ACharacter
@@ -19,12 +19,21 @@ class FLAPPYBIRD_API ABaseCharacter : public ACharacter
 public:
     ABaseCharacter(const FObjectInitializer& ObjInit);
 
-    virtual void Jump() override;
+    // Обработка оверлапа
+    UFUNCTION()
+    void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+                        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+                        bool bFromSweep, const FHitResult& SweepResult);
+
     virtual void Tick(float DeltaTime) override;
-    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    virtual void Jump() override;
+    virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
 
 protected:
-    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Components")
+    virtual void BeginPlay() override;
+
+    // Компоненты
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     UPaperFlipbookComponent* FlipbookComponent;
 
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
@@ -33,26 +42,10 @@ protected:
     UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
     UCameraComponent* CameraComponent;
 
-    virtual void BeginPlay() override;
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Collision")
+    UBoxComponent* CollisionBox;
 
 private:
+    // Генератор уровня
     ALevelGenerator* LevelGenerator;
 };
-
-// Убрать хардкод.
-// Перенести движение вперед и прыжок в movement component.
-// Убрать лишние include.
-// Для level generator добавить новый массив. 1 - хранит объекты для спавна на крыше 2- хранит объекты для спавна на полу (Нужно переворачивать деревья).
-// Добавить спавн крыши и пола.(Сейчас объекты поставлены вручную).
-// Запретить передвижение по оси Х.
-// Исправить ошибку, при которой камера ломается, если между камерой и персонажем есть объект.
-
-// Если птичка прыгает - спрайт поднимается, если птичка падает, спрайт опускается.
-
-// Лидерборд. 
-
-// Меню игры.
-
-// Написать readme
-
-// Собрать игру, и залить на google диск, что бы можно было скачать не собирая проект.
